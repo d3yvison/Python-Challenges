1a9cd01b04fabdbab883799df24a5ebd64999da2
d3yvison <109908428+d3yvison@users.noreply.github.com>
2026-04-06 12:47:42 -0300

    Update Ex 2 Ler data devolvendo resultado.py

--- Ex 2 Ler data devolvendo resultado.py
+++ Ex 2 Ler data devolvendo resultado.py
@@ -1,5 +1,5 @@
 #Ler dia, mês e ano de nascimento do usuário, devolvendo o resultado.
-day = input('Digite o dia do seu nascimento:')
-month = input('Digite o mês do seu nascimento:')
-year = input('Digite o ano de seu nascimento:')
+day = int(input('Digite o dia do seu nascimento:'))
+month = int(input('Digite o mês do seu nascimento:'))
+year = int(input('Digite o ano de seu nascimento:'))
 print ('Você nasceu dia' , day , 'de' , month , 'de' , year , ', correto?')
